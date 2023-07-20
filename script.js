ef6dd648ec46a50a31cdf1459ea6a5db7cd11837
Juan Pablo Cardozo <96211704+juanpabloow@users.noreply.github.com>
2023-07-19 21:31:05 -0500

    Sera que ya?

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const button = document.querySelector('.button-PD');
 button.addEventListener("click", e);
 
 function e(){
-    alert('A parte me quede esperando a que me pasara su numero >:(')
+    alert('No se para que te pedi el numero si no te escribo JAJAJA :D')
 }
